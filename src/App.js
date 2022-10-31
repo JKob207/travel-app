e30273b1a10001238730e1413021c6aa34446d45
Jakub Kobialka <kobialka.kontakt@gmail.com>
2022-10-31 21:26:35 +0100

    first commit and router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainPage from "./pages/MainPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App()
+{
+    return(
+        <Router>
+            <div>
+                <Header />
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/main-page" element={<MainPage />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
-
-export default App;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MainPage()
+{
+    return(
+        <div>
+            <h2>Main Page</h2>
+        </div>
+    );
+}
