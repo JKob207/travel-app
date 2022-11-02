8136f9674ca6a5648b6b0adf0f86ecca57a351d8
Jakub Kobialka <kobialka.kontakt@gmail.com>
2022-11-02 21:10:04 +0100

    added header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/header/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./style.scss";
 import HomePage from "./pages/home-page/HomePage";
 import MainPage from "./pages/main-page/MainPage";
 
 export default function App()
 {
+    const [notMainPage, setNotMainPage] = useState(false);
+
+    let location = useLocation();
+
+    useEffect(() => {
+        if(location.pathname !== "/") setNotMainPage(true);
+    }, [location])
+
     return(
-        <Router>
             <main className="main">
-                <Header />
+                <Header colorfulStyle={notMainPage}/>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/main-page" element={<MainPage />} />
                 </Routes>
             </main>
-        </Router>
     );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "./Header.scss";
-import LOGO from "./Travel_app_logo.png"
+import LOGO from "./Travel_app_logo.png";
+import PROFILE_PICTURE from "./profile-picture.png";
 
-export default function Header()
+export default function Header(props)
 {
+    const styles = {
+        backgroundColor: props.colorfulStyle ? "#F6AE2D" : "transparent" 
+    }
+
     return(
-        <header className="header">
+        <header className="header" style={styles}>
             <div className="header--logo">
                 <img src={LOGO} alt="Travel app logo" />
             </div>
-            <div className="header--profile">
-
-            </div>
+            {
+                props.colorfulStyle &&
+                <div className="header--profile">
+                    <span className="header--profile--user-welcome">Hi, Simon</span>
+                    <img className="header--profile--icon" src={PROFILE_PICTURE} alt="Profile picture" />
+                </div>
+            }
         </header>
     );
 }
