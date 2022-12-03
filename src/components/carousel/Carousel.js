52472e43290152386460345ea39613ddd5118075
Jakub Kobialka <kobialka.kontakt@gmail.com>
2022-12-03 18:05:09 +0100

    added search place

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -5,55 +5,100 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import data from "../../travelData";
 
-export default function Carousel()
+export default function Carousel({searchPlace})
 {
-    const [travelData, setTravelData] = React.useState(
-        data.map(travel => {
-            return <TravelCard
-                id={travel.id}
-                key={travel.id}
-                place={travel.place}
-                country={travel.country}
-                description={travel.description}
-                imgSrc={travel.imgSrc}
-            />
-        })
-    ); 
+  const [travelData, setTravelData] = React.useState(
+    data.map(travel => {
+        return <TravelCard
+            id={travel.id}
+            key={travel.id}
+            place={travel.place}
+            country={travel.country}
+            description={travel.description}
+            imgSrc={travel.imgSrc}
+        />
+    })
+);
+const [filterOn, setFilterOn] = React.useState(false); 
 
-    const carouselSettings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        initialSlide: 0,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-          ]
+  React.useEffect(() => {
+    searchPlaceFunc(searchPlace);
+  }, [searchPlace]);
+
+  const searchPlaceFunc = (place) =>
+  {
+    if(place !== "")
+    {
+      setTravelData(() => {
+        return data
+        .filter(travel => {
+          return travel.place === place
+        })
+        .map(travel => {
+          return <TravelCard
+              id={travel.id}
+              key={travel.id}
+              place={travel.place}
+              country={travel.country}
+              description={travel.description}
+              imgSrc={travel.imgSrc}
+          />
+        });
+      });
+      setFilterOn(true);
+    }else
+    {
+      setTravelData(() => {
+        return data
+        .map(travel => {
+          return <TravelCard
+              id={travel.id}
+              key={travel.id}
+              place={travel.place}
+              country={travel.country}
+              description={travel.description}
+              imgSrc={travel.imgSrc}
+          />
+        });
+      });
+      setFilterOn(false);
     }
+  }
+
+  const carouselSettings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: filterOn ? 1 : 3,
+      slidesToScroll: filterOn ? 1 : 3,
+      initialSlide: 0,
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: filterOn ? 1 : 3,
+              slidesToScroll: filterOn ? 1 : 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: filterOn ? 1 : 2,
+              slidesToScroll: filterOn ? 1 : 2,
+              initialSlide: filterOn ? 1 : 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+  }
 
     return(
         <div>
--- src/pages/main-page/MainPage.js
+++ src/pages/main-page/MainPage.js
@@ -6,16 +6,39 @@ import MAP from "../../images/map-dotted.png";
 
 export default function MainPage()
 {
+    const [searchedPlaceInput, setSearchedPlaceInput] = React.useState("");
+    const [searchPlace, setSearchPlace] = React.useState("");
+    
+
+    const handleChangeSearch = (event) =>
+    {
+        setSearchedPlaceInput(event.target.value);
+    }
+
+    const handleSearchPlace = () =>
+    {
+        setSearchPlace(searchedPlaceInput);
+    }
+
     return(
         <section className="main-page">
             <div className="main-page--search-bar">
-                <input placeholder='Try "New York"' className="main-page--search-input" />
-                <span className="search-icon"><img src={SEARCH_ICON} alt="search icon" /></span>
+                <input
+                    type="text" 
+                    placeholder='Try "Paris"' 
+                    className="main-page--search-input"
+                    onChange={handleChangeSearch}
+                    name="searchedPlaceInput"
+                    value={searchedPlaceInput}
+                />
+                <span className="search-icon" onClick={handleSearchPlace}>
+                    <img src={SEARCH_ICON} alt="search icon" />
+                </span>
             </div>
             <div className="main-page--discover-section">
                 <h2>Discover</h2>
                 <div className="main-page--discover-slider">
-                    <Carousel />
+                    <Carousel searchPlace={searchPlace} />
                 </div>
             </div>
             <div className="main-page--destinations">
